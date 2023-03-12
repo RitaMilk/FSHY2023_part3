b260c8666bdfe20fa42465358802c0971a98ead6
RitaMilk <80791613+RitaMilk@users.noreply.github.com>
2023-03-12 19:09:56 +0200

    eslint

--- index.js
+++ index.js
@@ -34,95 +34,95 @@ const errorHandler = (error, request, response, next) => {
     { id:4, name: 'Mary Poppendieck', number: '39-23-6423122' },
     { id:5, name: 'Delete Abramov', number: '12-43-234345' },
   ] */
-  let total = 0
-  let now = new Date()
-  let info= () =>{
-    let s=`<p>Phonebook has info for ${total} people</p>`
-    s=s.concat(`<p>${now}</p>`)
-    //console.log('s=',s)
-    return s
+let total = 0
+let now = new Date()
+let info= () => {
+  let s=`<p>Phonebook has info for ${total} people</p>`
+  s=s.concat(`<p>${now}</p>`)
+  //console.log('s=',s)
+  return s
 }
-   app.get('/api/phones', (request, response) => {
-    console.log('reitti /api/phones')
-    Phone.find({}).then(phones => {
-      console.log("phonebook:")
-      response.json(phones)
-    })
-  }) 
-  app.get('/api/info', (request, response) => {
-    //console.log('info=',info())
-    Phone.find({}).then(phones => {
-      total=phones.length
-      console.log("phonebook:")
-      response.json(info())
-    })
+app.get('/api/phones', (request, response) => {
+  console.log('reitti /api/phones')
+  Phone.find({}).then(phones => {
+    console.log('phonebook:')
+    response.json(phones)
   })
-  
-  app.get('/api/phones/:id', (request, response,next) => {
-      Phone.findById(request.params.id)
-        .then(phone =>{
-          if (phone) {
-            response.json(phone)
-          } else {
-            response.status(404).end()
-          }
-        })
-        .catch(error => next(error))
-        /* .catch(error => {
+})
+app.get('/api/info', (request, response) => {
+  //console.log('info=',info())
+  Phone.find({}).then(phones => {
+    total=phones.length
+    console.log('phonebook:')
+    response.json(info())
+  })
+})
+
+app.get('/api/phones/:id', (request, response,next) => {
+  Phone.findById(request.params.id)
+    .then(phone => {
+      if (phone) {
+        response.json(phone)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  /* .catch(error => {
           console.log(error)
           response.status(400).send({ error: 'malformatted id' })
         }) */
-        
-  })    
-  
-  app.delete('/api/phones/:id', (request, response,next) => {
-    Phone.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-  app.put('/api/phones/:id', (request, response,next) => {
-    const { name, number } = request.body
-    Phone.findByIdAndUpdate(
-      request.params.id,
-      {name,number},
-      {new:true,runValidators: true,context: 'query'}
-      )
-      .then(returnedPhone => {
-        console.log('kuku')
-        console.log("send phone",request.body.number)
-        console.log("updated phone",returnedPhone)
-        returnedPhone.number=request.body.number
-        response.json(returnedPhone)
-      })
-      .catch(error => next(error))
-  })
-  
-  app.post('/api/phones', (request, response,next) => {
-    const body = request.body
-  
-    if (body.name === undefined) {
-      return response.status(400).json({ error: 'name missing' })
-    }
-  
-    const phone = new Phone({
-      name: body.name,
-      number: body.number,
-    })
-  
-    phone.save().then(savedPhone => {
-      response.json(savedPhone)
+
+})
+
+app.delete('/api/phones/:id', (request, response,next) => {
+  Phone.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
     .catch(error => next(error))
-  })
- 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+})
+app.put('/api/phones/:id', (request, response,next) => {
+  const { name, number } = request.body
+  Phone.findByIdAndUpdate(
+    request.params.id,
+    { name,number },
+    { new:true,runValidators: true,context: 'query' }
+  )
+    .then(returnedPhone => {
+      console.log('kuku')
+      console.log('send phone',request.body.number)
+      console.log('updated phone',returnedPhone)
+      returnedPhone.number=request.body.number
+      response.json(returnedPhone)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/phones', (request, response,next) => {
+  const body = request.body
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
   }
-  
-  app.use(unknownEndpoint)
-  app.use(errorHandler)//part3-5 virheiden käsittely middleware
+
+  const phone = new Phone({
+    name: body.name,
+    number: body.number,
+  })
+
+  phone.save().then(savedPhone => {
+    response.json(savedPhone)
+  })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)//part3-5 virheiden käsittely middleware
 
 //const PORT = 3001
 //const PORT = process.env.PORT || 3001
--- models/phone.js
+++ models/phone.js
@@ -18,26 +18,26 @@ mongoose.connect(url)
   important: Boolean,
 }) */
 const phoneSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minlength: 3,
-      required: true
-    },
-    number: {
-      type: String,
-      minlength: 8,
-      validate: {
-        validator: function(v) {
-          return /\d{2,3}-\d{4,5}/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{4,5}/.test(v)
       },
-      required: [true, 'User phone number required']
+      message: props => `${props.value} is not a valid phone number!`
     },
-    
-  })
+    required: [true, 'User phone number required']
+  },
 
-  phoneSchema.set('toJSON', {
+})
+
+phoneSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
